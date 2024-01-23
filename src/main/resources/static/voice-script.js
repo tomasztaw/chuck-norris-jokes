bba25b1384e11d1e31e135dcc4f91c24565daa13
tomasztaw <tomasz.wegrzyn.wro@gmail.com>
2024-01-23 16:54:44 +0100

    Implement speak voice by JavaScript, random joke page is ready

---
+++ src/main/resources/static/voice-script.js
@@ -0,0 +1,30 @@
+let speech = new SpeechSynthesisUtterance();
+
+window.speechSynthesis.onvoiceschanged = () => {
+    const selectedVoice = window.speechSynthesis.getVoices().find(voice => voice.name === "Google UK English Male");
+
+    if (selectedVoice) {
+        speech.voice = selectedVoice;
+    } else {
+        console.warn("'Google UK English Male' voice is not available.");
+    }
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+    const speakButton = document.querySelector(".speak-button");
+    const nextJokeButton = document.querySelector(".next-joke-button");
+    const homePageButton = document.querySelector(".home-page-button");
+
+    speakButton.addEventListener("click", () => {
+        speech.text = document.querySelector("p").textContent;
+        window.speechSynthesis.speak(speech);
+    });
+
+    nextJokeButton.addEventListener("click", () => {
+        location.reload();
+    });
+
+    homePageButton.addEventListener("click", () => {
+        window.location.href = "/";
+    });
+});
